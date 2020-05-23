5aeeed2bc129b84353cb5dd5c2a35c8d83102894
medaymenTN <mohamedaymen.ourabi11@gmail.com>
2020-05-23 02:44:44 +0100

    adding prettier + eslint support and updating file structure

---
+++ src/services/product.services.ts
@@ -0,0 +1,12 @@
+import { API } from '../middlewares/middleware';
+type Product = {
+  id: string;
+  price: string;
+  label: string;
+};
+export const getAllProducts = (): Promise<Product[]> => {
+  return API.get('some url');
+};
+export const addProducts = (product: Product): Promise<any> => {
+  return API.post('some url', { product });
+};
